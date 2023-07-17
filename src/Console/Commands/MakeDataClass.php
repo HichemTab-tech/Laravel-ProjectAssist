34842e486ae4faae2d9cd0d5da7d7d9dce276a9c
HichemTab <konanhichemsinshi@gmail.com>
2023-07-17 17:58:06 +0100

    test

--- src/Console/Commands/MakeDataClass.php
+++ src/Console/Commands/MakeDataClass.php
@@ -1,6 +1,6 @@
 <?php /** @noinspection ALL */
 
-namespace HichemtabTech\LaravelProjectAssist\Console\Commands;
+namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
